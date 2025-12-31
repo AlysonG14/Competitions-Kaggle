0b133dd9427581344cc6fe0be53e3e599681a57c
Alyson Pereira <alyson.bosch.senai14@gmail.com>
2025-12-31 00:09:19 -0300

    Upload File 31/12 - feat: learning about Numpy

---
+++ titanic/03.py
@@ -0,0 +1,33 @@
+# numpy
+
+import numpy as np
+
+# notes from students
+
+notes = np.array([10,5,6,3,1,7])
+
+print('Media: ',np.mean(notes))
+print('Sum: ',np.sum(notes))
+print('Standard Deviation: ',np.std(notes, ddof=1))
+
+# another notes at school
+
+notes2 = np.array([
+    [2, 8, 7],
+    [7, 9, 6],
+    [10, 1, 5]
+])
+
+media_student = np.mean(notes2, axis=1)
+print(f'Media for student: {np.round(media_student, 2)}')
+
+
+deviation_student = np.std(notes2, axis=1)
+print(f'Standard Deviation for student: {np.round(deviation_student, 2)}')
+
+
+media_discipline = np.mean(notes2, axis=0)
+print(f'Media for Discipline: {np.round(media_discipline, 2)}')
+
+deviation_discipline = np.std(notes2, axis=0)
+print(f'Standard Deviation for discipline: {np.round(deviation_discipline, 2)}')
